fe8c07dba167296422ca90b23a9955c6261e8078
Dave Rigby <dave.r@pinecone.io>
2024-05-09 13:46:06 +0100

    Workload: Remove repetition of workload name
    We already define the name of each supported workload in the Workload
    enum - which is what is used to match the --workload argument. Remove
    the duplicated name declared for VectorWorkload.name property, just
    derive it from the Workload enum value.

--- vsb/workloads/__init__.py
+++ vsb/workloads/__init__.py
@@ -1,7 +1,8 @@
-from enum import Enum
+from enum import Enum, unique
 from .base import VectorWorkload
 
 
+@unique
 class Workload(Enum):
     """Set of supported workloads, the value is the string used to
     specify via --benchmark=
@@ -10,7 +11,12 @@ class Workload(Enum):
     Mnist = "mnist"
     MnistTest = "mnist-test"
 
-    def get_class(self) -> type[VectorWorkload]:
+    def build(self, **kwargs) -> VectorWorkload:
+        """Construct an instance of VectorWorkload based on the value of the enum."""
+        cls = self._get_class()
+        return cls(self.value, **kwargs)
+
+    def _get_class(self) -> type[VectorWorkload]:
         """Return the VectorWorkload class to use, based on the value of the enum"""
         match self:
             case Workload.Mnist:
