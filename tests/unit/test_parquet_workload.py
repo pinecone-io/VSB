504c7b2957e382e5daac6165880a6a9cf6812fb1
Dave Rigby <dave.r@pinecone.io>
2024-07-05 16:20:00 +0100

    Speedup SubsetWorkload top_k calculation 9x via bulk recalc
    Refactor top_k recalculation to process all queries at the same time,
    instead of one at a time. This allows us to reuse state (e.g. stacked
    ndarray of records) across updating multiple queries.
    
    This results is a further 9x improvement in recalcution throughput /
    latency - compared to previous version:
    
        ----------------------------------------------------------------------------------------------- benchmark: 2 tests -----------------------------------------------------------------------------------------------
        Name (time in ms)                                     Min                Max               Mean            StdDev             Median               IQR            Outliers       OPS            Rounds  Iterations
        ------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
        test_topk_recalc_mnist_euclidean (NOW)             2.3764 (1.0)       3.3675 (1.0)       2.6242 (1.0)      0.1281 (1.0)       2.6046 (1.0)      0.1778 (1.0)          69;3  381.0616 (1.0)         272           1
        test_topk_recalc_mnist_euclidean (0015_numpy)     24.5242 (10.32)    26.4318 (7.85)     25.4137 (9.68)     0.3996 (3.12)     25.4111 (9.76)     0.4818 (2.71)          9;1   39.3489 (0.10)         35           1
        ------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
    
    In total (Comparing to original version prior to any optimisation) we see a ~160x improvement:
    
        --------------------------------------------------------------------------------------------------- benchmark: 2 tests ---------------------------------------------------------------------------------------------------
        Name (time in ms)                                        Min                 Max                Mean             StdDev              Median                IQR            Outliers       OPS            Rounds  Iterations
        --------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
        test_topk_recalc_mnist_euclidean (NOW)                2.3219 (1.0)        3.0723 (1.0)        2.5731 (1.0)       0.0858 (1.0)        2.5667 (1.0)       0.0918 (1.0)         59;10  388.6417 (1.0)         268           1
        test_topk_recalc_mnist_euclidean (0013_baselin)     414.1057 (178.35)   445.5805 (145.03)   428.1109 (166.38)   13.2473 (154.31)   427.6140 (166.60)   22.7173 (247.38)        2;0    2.3358 (0.01)          5           1
        --------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------

--- tests/unit/test_parquet_workload.py
+++ tests/unit/test_parquet_workload.py
@@ -18,35 +18,42 @@ class TestSubsetWorkloadKNN:
         ]
     )
 
-    queries = [
-        SearchRequest(values=[10, 0.5, 0], top_k=3),
-        SearchRequest(values=[-1, 0.1, 1], top_k=3),
-        SearchRequest(values=[0, 5, 2], top_k=2),
-    ]
+    queries = pandas.DataFrame(
+        [
+            {"values": [10, 0.5, 0], "top_k": 3},
+            {"values": [-1, 0.1, 1], "top_k": 3},
+            {"values": [0, 5, 2], "top_k": 2},
+        ]
+    )
 
     def calc_knn(self, metric: DistanceMetric, q: SearchRequest) -> list[str]:
         return ParquetSubsetWorkload.calc_k_nearest_neighbors(self.records, metric, q)
 
+    def recalculate_neighbors(self, metric: DistanceMetric) -> pandas.DataFrame:
+        return ParquetSubsetWorkload.recalculate_neighbors(
+            self.records, self.queries, metric
+        )
+
     def test_cosine(self):
         # Test KNN recalculation for cosine distance metric.
-        metric = DistanceMetric.Cosine
+        query_results = self.recalculate_neighbors(DistanceMetric.Cosine)
 
-        assert self.calc_knn(metric, self.queries[0]) == ["a", "c", "b"]
-        assert self.calc_knn(metric, self.queries[1]) == ["d", "e", "f"]
-        assert self.calc_knn(metric, self.queries[2]) == ["b", "e"]
+        assert query_results.iloc[0] == ["a", "c", "b"]
+        assert query_results.iloc[1] == ["d", "e", "f"]
+        assert query_results.iloc[2] == ["b", "e"]
 
     def test_dot_product(self):
         # Test KNN recalculation for cosine dot product metric.
-        metric = DistanceMetric.DotProduct
+        query_results = self.recalculate_neighbors(DistanceMetric.DotProduct)
 
-        assert self.calc_knn(metric, self.queries[0]) == ["c", "a", "e"]
-        assert self.calc_knn(metric, self.queries[1]) == ["e", "d", "f"]
-        assert self.calc_knn(metric, self.queries[2]) == ["e", "c"]
+        assert query_results.iloc[0] == ["c", "a", "e"]
+        assert query_results.iloc[1] == ["e", "d", "f"]
+        assert query_results.iloc[2] == ["e", "c"]
 
     def test_euclidean(self):
         # Test KNN recalculation for euclidean metric.
-        metric = DistanceMetric.Euclidean
+        query_results = self.recalculate_neighbors(DistanceMetric.Euclidean)
 
-        assert self.calc_knn(metric, self.queries[0]) == ["c", "a", "b"]
-        assert self.calc_knn(metric, self.queries[1]) == ["d", "b", "f"]
-        assert self.calc_knn(metric, self.queries[2]) == ["e", "f"]
+        assert query_results.iloc[0] == ["c", "a", "b"]
+        assert query_results.iloc[1] == ["d", "b", "f"]
+        assert query_results.iloc[2] == ["e", "f"]
