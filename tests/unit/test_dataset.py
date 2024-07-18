a00a3affd20801fb652cff0349fcc5888ef49d31
Jonathan Xu <jonathanzhxu@gmail.com>
2024-07-18 14:57:31 -0400

    add tests for uniform multi-user single-file batch sizes

--- tests/unit/test_dataset.py
+++ tests/unit/test_dataset.py
@@ -22,11 +22,32 @@ class TestDataset:
         # chunk sizes are uneven.
         num_chunks = 7
         total = 0
+        min_chunk_length = None
+        max_chunk_length = None
         for chunk_id in range(num_chunks):
             iter = dataset.get_batch_iterator(num_chunks, chunk_id, 100)
             chunk_total = sum([len(batch) for batch in iter])
             total += chunk_total
+            # Splitting a single parquet file into multiple chunks should yield
+            # similar sized chunks.
+            min_chunk_length = min(min_chunk_length, chunk_total) if min_chunk_length else chunk_total
+            max_chunk_length = max(max_chunk_length, chunk_total) if max_chunk_length else chunk_total
+        assert max_chunk_length - min_chunk_length <= 1
         assert total == 60000
+    
+    def test_get_batch_iter_multifile(self):
+        # Test a batch iter for multiple chunks yields the entire dataset,
+        # when the dataset is split across multiple files.
+        dataset = Dataset("nq768", cache_dir=default_cache_dir)
+        # Choosing num_chunks which is not a factor of dataset size, so
+        # chunk sizes are uneven.
+        num_chunks = 7
+        total = 0
+        for chunk_id in range(num_chunks):
+            iter = dataset.get_batch_iterator(num_chunks, chunk_id, 100)
+            chunk_total = sum([len(batch) for batch in iter])
+            total += chunk_total
+        assert total == 2_680_893
 
     def test_get_batch_iter_limit(self):
         # Test a batch iter for multiple chunks yields the entire dataset when
