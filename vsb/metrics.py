531c2e98573f61aa9d3e94f48f4e7edf7dc1c1d0
Jonathan Xu <jonathanzhxu@gmail.com>
2024-08-06 15:00:23 -0400

    Add MAP and MRR metrics reporting
    Adds Mean Average Precision and Mean Reciprocal Rank as metrics.
    Calculates Average Precision and Reciprocal Rank for each query
    individually, like Recall, and spits out the full distribution
    including the "MeanXX" statistic in the "Mean" column.

--- vsb/metrics.py
+++ vsb/metrics.py
@@ -40,7 +40,62 @@ class Recall(Metric):
         return matches / len(expected)
 
 
-METRICS = (Recall,)
+class AveragePrecision(Metric):
+    """Measure the average precision of the given request / result."""
+
+    @staticmethod
+    def name():
+        return "Average Precision"
+
+    @staticmethod
+    def measure(request: SearchRequest, results: list[str]) -> float:
+        return AveragePrecision._calculate(results, request.neighbors[: request.top_k])
+
+    @staticmethod
+    def _calculate(actual: list[str], expected: list[str]) -> float:
+        if not expected:
+            if not actual:
+                # If we expect [] and receive [], the result is fully correct.
+                return 1.0
+            # If we expect [] and receive vectors, the result is (fully) incorrect.
+            return 0.0
+        relevant = set(expected)
+        num_true_positives = 0
+        precision = 0
+        for i, result in enumerate(actual):
+            if result in relevant:
+                num_true_positives += 1
+            precision += num_true_positives / (i + 1)
+        return precision / len(expected)
+
+
+class ReciprocalRank(Metric):
+    """Measure the reciprocal rank of the given request / result."""
+
+    @staticmethod
+    def name():
+        return "Reciprocal Rank"
+
+    @staticmethod
+    def measure(request: SearchRequest, results: list[str]) -> float:
+        return ReciprocalRank._calculate(results, request.neighbors[: request.top_k])
+
+    @staticmethod
+    def _calculate(actual: list[str], expected: list[str]) -> float:
+        if not expected:
+            if not actual:
+                # If we expect [] and receive [], the result is fully correct.
+                return 1.0
+            # If we expect [] and receive vectors, the result is (fully) incorrect.
+            return 0.0
+        relevant = set(expected)
+        for i, result in enumerate(actual):
+            if result in relevant:
+                return 1 / (i + 1)
+        return 0.0
+
+
+METRICS = (Recall, AveragePrecision, ReciprocalRank)
 """Metrics to be calculated for each request."""
 
 
