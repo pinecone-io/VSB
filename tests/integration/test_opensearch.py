7882845778490ffd71ab8878bd14da6dea97a72d
Dave Rigby <dave.r@pinecone.io>
2025-04-11 17:09:11 +0100

    Use correct test password for CI job

--- tests/integration/test_opensearch.py
+++ tests/integration/test_opensearch.py
@@ -9,7 +9,7 @@ def spawn_vsb_opensearch(workload, timeout=60, extra_args=None, **kwargs):
     its stdout and stderr.
     """
     extra_env = {
-        "VSB__PGVECTOR_USERNAME": "postgres",
-        "VSB__PGVECTOR_PASSWORD": "postgres",
+        "VSB__OPENSEARCH_USERNAME": "admin",
+        "VSB__OPENSEARCH_PASSWORD": "opensearch",
     }
     return spawn_vsb_inner("opensearch", workload, timeout, extra_args, extra_env)
